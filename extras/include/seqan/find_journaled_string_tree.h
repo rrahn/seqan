d1c359716a81de6547a8e2c446237934f91228a6
René Rahn <rene.rahn@fu-berlin.de>
2014-07-08 15:21:51 +0200

    [INTERNAL] Fixes misspelled file name in include directive.

--- extras/include/seqan/find_journaled_string_tree.h
+++ extras/include/seqan/find_journaled_string_tree.h
@@ -70,7 +70,7 @@
 // Approximate Search.
 // ===========================================================================
 
-#include <seqan/find_journaled_string_tree/find_Extension_myers_ukkonen.h>
+#include <seqan/find_journaled_string_tree/find_extension_myers_ukkonen.h>
 
 
 #endif // EXTRAS_INCLUDE_SEQAN_FIND_JOURNALED_STRING_TREE_
