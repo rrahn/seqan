70e810f53d096b3312e818840e898bd73d3d81db
rrahn <rene.rahn@fu-berlin.de>
2016-07-28 15:21:16 +0200

    enable test

--- util/cmake/SeqAnBuildSystem.cmake
+++ util/cmake/SeqAnBuildSystem.cmake
@@ -322,11 +322,11 @@ macro (seqan_add_app_test APP_NAME)
     endif ()
     find_package (PythonInterp)
     if (PYTHONINTERP_FOUND)
-    #   add_test (NAME app_test_${APP_NAME}${ARGV1}
-    #             COMMAND ${PYTHON_EXECUTABLE}
-    #                     ${CMAKE_CURRENT_SOURCE_DIR}/tests/run_tests${ARGV1}.py
-    #                     ${_VALGRIND_FLAG}
-    #                     ${CMAKE_SOURCE_DIR} ${CMAKE_BINARY_DIR})
+      add_test (NAME app_test_${APP_NAME}${ARGV1}
+                COMMAND ${PYTHON_EXECUTABLE}
+                        ${CMAKE_CURRENT_SOURCE_DIR}/tests/run_tests${ARGV1}.py
+                        ${_VALGRIND_FLAG}
+                        ${CMAKE_SOURCE_DIR} ${CMAKE_BINARY_DIR})
     endif (PYTHONINTERP_FOUND)
 endmacro (seqan_add_app_test APP_NAME)
 
